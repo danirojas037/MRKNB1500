536ab8a4b5d0f89e1600803063bac3626fab92fc
danirojas037 <87932867+danirojas037@users.noreply.github.com>
2022-09-14 22:56:57 +0200

    Add files via upload

---
+++ MKR1500_Azure_Final/arduino_secrets.h
@@ -0,0 +1,8 @@
+// NB settings
+#define SECRET_PINNUMBER ""
+
+// Fill in the hostname of your Azure IoT Hub broker
+#define SECRET_BROKER    "iotc-bb30c02b-57d3-4442-b7b0-4abc43106b03.azure-devices.net"
+
+// Fill in the Connection string for your device
+#define CONN_STRING "HostName=iotc-bb30c02b-57d3-4442-b7b0-4abc43106b03.azure-devices.net;DeviceId=19qf5t6cjmf;SharedAccessKey=/plqlhTZ42LRFGTzR8wj5LYKupS3KCaEHI5PdZTanVI="
